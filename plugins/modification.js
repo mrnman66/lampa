9da69bb7b498d9ea3192a6f32ac2e7a520351132
bylampa <157304712+bylampa@users.noreply.github.com>
2024-08-29 18:28:10 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -6,3 +6,18 @@
 	    'https://bylampa.github.io/addon.js?v=' + Math.random(),
 	    'https://bylampa.github.io/themes.js?v=' + Math.random()
    ], function () {});
+
+   function start_set(){
+           Lampa.Storage.set('set','true');
+           Lampa.Storage.set('keyboard_type', 'integrate');
+           Lampa.Storage.set('start_page', 'main');
+           Lampa.Storage.set('source', 'cub');
+           Lampa.Storage.set('background', 'false');
+	   Lampa.Storage.set('animation', 'false');
+	   Lampa.Storage.set('mask', 'false');
+	   Lampa.Storage.set('player_normalization', 'true');
+	   Lampa.Storage.set('player_timecode', 'ask');
+	   Lampa.Storage.set('screensaver', 'false');
+           Lampa.Storage.set('pages_save_total', '3');
+	   location.reload()
+    } 
