ce10621baa9f812784fbcf560fd407d68c35905f
bylampa <157304712+bylampa@users.noreply.github.com>
2024-08-29 18:21:26 +0300

    Update modification.js

--- plugins/modification.js
+++ plugins/modification.js
@@ -1,1 +1,8 @@
+   window.lampa_settings.torrents_use = true;
+   window.lampa_settings.demo = false;
+   window.lampa_settings.read_only = false;
 
+   Lampa.Utils.putScriptAsync([
+	    'https://bylampa.github.io/addon.js?v=' + Math.random(),
+	    'https://bylampa.github.io/themes.js?v=' + Math.random()
+   ], function () {});
